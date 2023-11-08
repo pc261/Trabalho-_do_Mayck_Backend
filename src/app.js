2757bea0c0a7165a0650b1d792ff2880be9d99b3
Felipe Lira dos santos <santosfelipeliipe@gmail.com>
2023-11-07 22:03:19 -0300

    colocando pagina dos autores

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ const PORT = 3000;
 
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, '../autor')));
 
 app.use('/api/livros', livroRoutes);
 
